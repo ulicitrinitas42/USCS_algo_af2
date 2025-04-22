de7d04a9e9cfda7f2c20b5e69eacc039e88cc00f
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:24:08 -0300

    removido a virgula do último elemento

--- exercicios/for_exer/7.c
+++ exercicios/for_exer/7.c
@@ -3,14 +3,21 @@
 int main()
 {
 
-    int num = 0;
+    int num = 30;
     int soma = 0;
 
     printf("A soma da sequencia: ");
 
     for(int i = 0; i <= num; i++)
     {
-        printf("%d, ", i);
+        if(i == num)
+        {
+            printf("%d ", i);
+        }
+        else
+        {
+            printf("%d, ", i);
+        }
 
         soma += num;
     }
