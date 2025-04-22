e07e79904994e2bd593da497be737b33d4ba6a8b
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:10:06 -0300

    criado loop de potencias

--- exercicios/for_exer/6.c
+++ exercicios/for_exer/6.c
@@ -5,7 +5,26 @@
 int main()
 {
 
+    int num = 2;
+    int soma = 0;
 
+    for(int i = 0; i <= num; i++)
+    {
+
+        soma += pow(num, i);
+
+        if(i == num)
+        {
+            printf("%d^%d ", num, i);
+        }
+        else
+        {
+            printf("%d^%d + ", num, i);
+        }
+        
+    }
+
+    puts("");
 
     return 0;
     
