11c9a795290bf23ee7fb35aedbb55bd1f37ddee3
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 19:56:00 -0300

    cria a lista de numeros

--- exercicios/for_exer/5.c
+++ exercicios/for_exer/5.c
@@ -2,5 +2,20 @@
 
 int main()
 {
+
+    puts("Imprimindo os numeros...");
+
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 6 == 0)
+        {
+            continue;
+        }
+
+        printf("%d ", i);
+    }
+
+    puts("");
+
     return 0;
 }
