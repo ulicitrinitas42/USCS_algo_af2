99f18b06ad6c07aa6ad080bd666b5562e8145831
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 19:45:59 -0300

    criado loop pra somar os digitos

--- exercicios/while_exer/7.c
+++ exercicios/while_exer/7.c
@@ -3,7 +3,18 @@
 int main()
 {
 
-    
+    int soma = 0;
+    int num = 0;
+
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        soma += num % 10;
+        num /= 10;
+    }
+
+    printf("A soma dos digitos eh %d\n", soma);
 
     return 0;
 }
