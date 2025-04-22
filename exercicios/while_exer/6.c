ab89c1f886a14f88e3d9b24b83447f995ad79c18
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 19:35:57 -0300

    verifica qual numero digitado é o maior

--- exercicios/while_exer/6.c
+++ exercicios/while_exer/6.c
@@ -2,5 +2,28 @@
 
 int main()
 {
+
+    int n = 0;
+    int maior = 0;
+
+    while(1)
+    {
+        printf("digite um número [escreva -999 pra sair]: ");
+        scanf("%d", &n);
+
+        if(n == -999)
+        {
+            break;
+        }
+
+        if(maior < n)
+        {
+            maior = n;
+        }
+
+    }
+
+    printf("O maior numero eh %d\n", maior);
+
     return 0;
 }
