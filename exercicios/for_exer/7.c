7160d646f6e1a0f1b0f0eb4953a6ff48acbd5688
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:20:49 -0300

    criado o gerador de sequencia

--- exercicios/for_exer/7.c
+++ exercicios/for_exer/7.c
@@ -2,5 +2,20 @@
 
 int main()
 {
+
+    int num = 0;
+    int soma = 0;
+
+    printf("A soma da sequencia: ");
+
+    for(int i = 0; i <= num; i++)
+    {
+        printf("%d, ", i);
+
+        soma += num;
+    }
+
+    printf("eh %d\n", soma);
+
     return 0;
 }
