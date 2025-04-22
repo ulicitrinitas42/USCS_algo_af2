ee5cb153b807af4a600935b55565d285ee5c7ff5
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 18:00:17 -0300

    escreve os tres numeros

--- exercicios/5.c
+++ exercicios/5.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+#define tam 3
+
 int main()
 {
-    
 
+    int nums[tam];
+
+    puts("Digite 3 numeros: ");
+
+    for (int i = 0; i < tam; i++) 
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("nums: %d %d %d\n", nums[0], nums[1], nums[2]);
 
     return 0;
 }
