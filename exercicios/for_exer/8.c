9956990c850180453dc3b6554f94fae642926792
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:32:59 -0300

    criado o calculo do fatorial

--- exercicios/for_exer/8.c
+++ exercicios/for_exer/8.c
@@ -2,5 +2,25 @@
 
 int main()
 {
+    int num = 0;
+    int fat = 0;
+
+    if(num == 0)
+    {
+        printf("O fatorial de 0 é 1\n");
+    }
+    else if(num == 1)
+    {
+        printf("O fatorial de 1 é 1\n");
+    }
+    else
+    {
+        for(int i = 1; i < num; i++)
+        {
+            fat *= i;
+        }
+    }
+
+
     return 0;
 }
