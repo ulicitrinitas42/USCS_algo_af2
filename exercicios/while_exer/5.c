d32838a315f5d840e2b5f103a9788e80eb4f0180
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 18:59:53 -0300

    criado a inicialização do vetor

--- exercicios/while_exer/5.c
+++ exercicios/while_exer/5.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+#define tam 5
+
 int main()
 {
-    
+    int nums[tam];
+    int i = 0;
+    int soma = 0;
+
+    puts("Digite cinco números (1 por <enter>): ");
+
+    while(i < tam)
+    {
+        scanf("%d", &nums[i]);
+        i++;
+    }
+
+    i = 0;
+
+    while(i < tam)
+    {
+        
+    }
+
 }
 
