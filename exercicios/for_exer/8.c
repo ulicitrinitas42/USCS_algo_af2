89cf7c5b45fb878ceda2bd5fdae360c417bef5a2
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:36:28 -0300

    removido os if

--- exercicios/for_exer/8.c
+++ exercicios/for_exer/8.c
@@ -3,23 +3,14 @@
 int main()
 {
     int num = 0;
-    int fat = 0;
+    int fat = 1;
 
-    if(num == 0)
-    {
-        printf("O fatorial de 0 é 1\n");
-    }
-    else if(num == 1)
-    {
-        printf("O fatorial de 1 é 1\n");
-    }
-    else
-    {
-        for(int i = 1; i < num; i++)
-        {
-            fat *= i;
-        }
-    }
+    printf("escreva o numero que quer o fatorial: ");
+    scanf("%d", &num);
+
+    
+
+    printf("O fatorial de %d eh %d\n", num, fat);
 
 
     return 0;
