eb6df732b9174b975cbf1bad4ae4d39d071509dc
ulicitrinitas <uli.citrinitas@outlook.com>
2025-04-22 20:15:29 -0300

    adicionado o scanf

--- exercicios/for_exer/6.c
+++ exercicios/for_exer/6.c
@@ -5,8 +5,11 @@
 int main()
 {
 
-    int num = 3;
+    int num = 0;
     int soma = 0;
+
+    printf("Escreva qual numero quer ver a sequencia de potencias: ");
+    scanf("%d", &num);
 
     for(int i = 0; i <= num; i++)
     {
